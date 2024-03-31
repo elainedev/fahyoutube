bdf47baf19b4ec820fc44d5ea481f1c3ddbd0cf5
Elaine Wang <elainewang.cal@gmail.com>
2024-03-31 03:17:50 -0700

    code

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var awardDiv = document.getElementById("award");
+
+  awardDiv.addEventListener("click", function () {
+    window.open("https://www.youtube.com/@foilarmsandhog");
+  });
+});
